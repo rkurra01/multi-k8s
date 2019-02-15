0fd23dfa03396a00fe5d8819295e9d9642c455d9
Rajesh Kurra <rkurra01@gmail.com>
2019-02-15 05:22:27 -0600

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  //Comment the below to prevent test failues due to lack of backend during test exec
+  //const div = document.createElement('div');
+  //ReactDOM.render(<App />, div);
+  //ReactDOM.unmountComponentAtNode(div);
+});
